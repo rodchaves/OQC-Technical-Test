ea257962e942785526e4d3620cdf0720edc00a0e
Rodrigo Chaves <rodrigoogchaves@gmail.com>
2022-06-09 02:59:16 -0300

    Add addition of gates and test for addition
    Added the first optimization step of the circuit which is addition
    of gates with the same axis of rotation. Also, added numerous tests
    to check if the addition is working.

--- source/circuit.py
+++ source/circuit.py
@@ -18,7 +18,7 @@ class Circuit:
             self.gates = gates
 
     @classmethod
-    def from_string(self, list_of_gates: list):
+    def from_string(cls, list_of_gates: list):
         """
         Method that transforms a list of gates into a Circuit.
 
@@ -38,6 +38,77 @@ class Circuit:
         for gates_string in splitted_gates:
             gates.append(Gate.from_string(gates_string))
 
-        return Circuit(gates = gates)
+        return cls(gates = gates)
+    
+    def _sum_rotations_same_axis(self):
+        """
+        Private function to sum gates with the same axis that will be
+        used in the optimization of the whole circuit.
+
+        Parameters
+        ----------
+        circuit: Circuit
+
+        Returns
+        --------
+        circuit: Circuit
+
+        """
+        list_of_gates = self.gates
+        final_circuit = []
+        temp_gate = list_of_gates[0]
+        list_of_gates.pop(0)
+
+        for gate in list_of_gates:
+            if temp_gate.axis == gate.axis:
+                temp_gate = temp_gate + gate
+            else:
+                if temp_gate.angle != 0:
+                    final_circuit.append(temp_gate)
+                temp_gate = gate
+
+        final_circuit.append(temp_gate)
+        
+        return Circuit(gates = final_circuit)
+
+    def _check_reflections(self):
+        list_of_gates = self.gates
+        final_circuit = []
+        temp_gate = list_of_gates[0]
+        list_of_gates = list_of_gates.pop(0)
+
+        for gate in list_of_gates:
+            if temp_gate.axis != gate.axis and gate.angle == 180:
+                final_circuit.append(gate)
+                temp_gate = Gate(axis = gate.axis, angle = -gate.angle)
+            else:
+                final_circuit.append(temp_gate)
+                temp_gate = gate
+        
+        return Circuit(gates = final_circuit)
+
+    def optimizationXY(self):
+        """
+        Method to optimize the circuit that will add sequential gates with
+        the same axis of rotation and do reflections when Y(180) or X(180)
+        are present.
+
+        Parameters
+        ----------
+        circuit: Circuit
+
+        Returns
+        -------
+        circuit: Circuit
+
+        """
+        #self.gates = _check_reflections(self).gates
+        self = self._sum_rotations_same_axis()
+        
+        return self
+
+
+
+
 
     
--- test/test_cricuit.py
+++ test/test_cricuit.py
@@ -9,3 +9,41 @@ class TestCircuit:
             Gate(axis = 'X', angle = 90),
             Gate(axis = 'Y', angle = 180)
         ]
+    
+    def test_sum_of_gates(self):
+        circuit1 = Circuit.from_string("X(90),X(90),X(90)")
+        circuit2 = Circuit.from_string("X(90),Y(90),X(90)")
+        circuit3 = Circuit.from_string("X(90),X(-90),X(90)")
+        circuit4 = Circuit.from_string("X(90),X(90),Y(90)")
+        circuit5 = Circuit.from_string("X(30),X(90)")
+        circuit6 = Circuit.from_string("X(90)")
+        
+        circuit1 = circuit1.optimizationXY()
+        circuit2 = circuit2.optimizationXY()
+        circuit3 = circuit3.optimizationXY()
+        circuit4 = circuit4.optimizationXY()
+        circuit5 = circuit5.optimizationXY()
+        circuit6 = circuit6.optimizationXY()
+
+        
+        assert circuit1.gates == [
+            Gate(axis = 'X', angle = 270)
+        ]
+        assert circuit2.gates == [
+            Gate(axis = 'X', angle = 90),
+            Gate(axis = 'Y', angle = 90),
+            Gate(axis = 'X', angle = 90)
+        ]
+        assert circuit3.gates == [
+            Gate(axis = 'X', angle = 90)
+        ]
+        assert circuit4.gates == [
+            Gate(axis = 'X', angle = 180),
+            Gate(axis = 'Y', angle = 90)
+        ]
+        assert circuit5.gates == [
+            Gate(axis = 'X', angle = 120)
+        ]
+        assert circuit6.gates == [
+            Gate(axis = 'X', angle = 90)
+        ]
