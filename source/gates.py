4582cb75896b15d7918f5a6bf667b7440ee54164
Rodrigo Chaves <rodrigoogchaves@gmail.com>
2022-06-08 22:38:35 -0300

    Add Gate class
    Added the Gate class with axes and angles to construct the circuit later
    on associated with a sequence of rotations.

---
+++ source/gates.py
@@ -0,0 +1,44 @@
+from dataclasses import dataclass
+import re
+
+
+@dataclass
+class Gate:
+    """
+    A class to represent gates. Dataclass was used so we can compare two entries
+    of the class for later usage.
+
+    Attributes
+    ----------
+    axis: axis of rotation
+    angle: angle of rotation in angles
+
+    Methods
+    -------
+
+    """
+    axis: str
+    angle: int
+
+    @classmethod
+    def from_string(cls, gate:str) -> Gate:
+        """
+        Construct the gate from a given string.
+
+        Parameters
+        ----------
+        gate: str
+
+        Returns
+        -------
+        gate: Gate
+
+        """
+        splitted_string = re.split("\(|\)", gate.strip())
+        axis = splitted_list[0]
+        angle = int(splitted_list[1])
+
+        return cls(axis=axis,angle=angle)
+    
+
+
