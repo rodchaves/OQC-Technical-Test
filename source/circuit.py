dd71d4377100108ff69b7172ecb32f9124adf3d9
Rodrigo Chaves <rodrigoogchaves@gmail.com>
2022-06-08 23:23:25 -0300

    Add the class circuit
    Added the class circuit that will be a list of Gates associated with
    an inputed string.

---
+++ source/circuit.py
@@ -0,0 +1,43 @@
+from source.gates import Gate
+import re
+
+class Circuit:
+    """
+    Class defined to create the circuit associated to a list of with gates. 
+
+    Attributes
+    -----------
+    gates: Gate
+
+    Methods
+    -------
+
+
+    """
+    def __init__(self, gates: list):
+            self.gates = gates
+
+    @classmethod
+    def from_string(self, list_of_gates: list):
+        """
+        Method that transforms a list of gates into a Circuit.
+
+        Parameters
+        ----------
+        list_of_gates: list
+
+        Returns
+        -------
+        Circuit: Circuit
+
+        """
+        splited_gates = re.split(',', list_of_gates)
+
+        gates = []
+
+        from gates_string in splitted_gates:
+            gates.append(Gate.from_string(gates_string))
+
+        return Circuit(gates = gates)
+
+    
